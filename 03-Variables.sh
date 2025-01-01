c9e7aee6a28aa103caded21c4865719ee0f84274
Deepikaaws189 <kathalakshmideepika@gmail.com>
2025-01-01 22:44:01 +0530

    Variables defined

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: Im fine,how are you $PERSON1"
